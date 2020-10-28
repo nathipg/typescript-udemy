0ad6dac73fa1f1072222415c8ef4d1057e763f42
Nathália Pissuti Gomes <nathalia.pissuti@venturus.org.br>
2020-10-28 16:31:27 -0300

    Object types

--- project/app.ts
+++ project/app.ts
@@ -1,15 +1,6 @@
-const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
-    const result = n1 + n2;
-    if(showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
+const person = {
+    name: 'Pissuti',
+    age: 23
 };
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person);
