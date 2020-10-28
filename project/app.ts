67996b501251f0a6fcd189e137c6fd3b30a12043
Nathália Pissuti Gomes <nathalia.pissuti@venturus.org.br>
2020-10-28 16:42:37 -0300

    Arrays types

--- project/app.ts
+++ project/app.ts
@@ -1,6 +1,17 @@
 const person = {
     name: 'Pissuti',
-    age: 23
+    age: 23,
+    hobbies: [
+        'Sports',
+        'Cooking'
+    ]
 };
 
+let favoriteActivities: any[]; // Avoid
+favoriteActivities = ['Sports', 30];
+
 console.log(person);
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
