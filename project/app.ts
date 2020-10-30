b7aaefed093a3ecf2f907482735babcac878b8d0
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-10-30 12:31:41 -0300

    Functions as types

--- project/app.ts
+++ project/app.ts
@@ -7,3 +7,9 @@ const printResult = (num: number): void => {
 }
 
 printResult(add(5, 12));
+
+let combineValues: (n1: number, n2: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8 , 8));
