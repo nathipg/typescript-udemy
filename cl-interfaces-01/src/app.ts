0f275915031026c18b87fcc6d37a5cafd79701fd
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-01-17 16:33:38 -0300

    Abstract Classes

--- cl-interfaces-01/src/app.ts
+++ cl-interfaces-01/src/app.ts
@@ -1,18 +1,16 @@
-class Department {
+abstract class Department {
   static fiscalYear = 2020;
   /* private name: string; */
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
   }
 
   static createEmployee(name: string) {
     return {name: name};
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
-  }
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -30,6 +28,10 @@ class ITDepartment extends Department {
   constructor(id: string, admins: string[]) {
     super(id, 'IT');
     this.admins = admins;
+  }
+
+  describe() {
+    console.log(`IT Department (${this.id}): ${this.name}`);
   }
 }
 
@@ -69,7 +71,13 @@ class AccountingDepartment extends Department {
   printReports() {
     console.log(this.reports);
   }
+
+  describe() {
+    console.log(`Accounting Department (${this.id}): ${this.name}`);
+  }
 }
+
+
 
 const employee1 = Department.createEmployee('Max');
 console.log(employee1, Department.fiscalYear);
