19eb0affb53e773bd0cde2b4fb0da373c2e7062b
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-01-18 19:49:19 -0300

    Return and change a class in a class decorator

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -8,15 +8,19 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
     console.log('TEMPLATE FACTORY');
-    return function(constructor: any) {
-        console.log('RENDER TEMPLATE');
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if(hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name;
+    return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                super();
+                console.log('RENDER TEMPLATE');
+                const hookEl = document.getElementById(hookId);
+                if(hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name;
+                }
+            }
         }
-    }
+    };
 }
 
 @Logger('LOGGING - PERSON')
