dd556d926854bab17d0f235a723df9270ec953ee
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-01-18 19:59:44 -0300

    Autobind decorator

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -84,3 +84,31 @@ class Product {
 
 const p1 = new Product('Book', 19);
 const p2 = new Product('Book 2', 29);
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'It Works';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button');
+button?.addEventListener('click', p.showMessage);
