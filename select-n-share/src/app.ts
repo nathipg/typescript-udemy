697f13ca278b9342949af4a01db67dc50731d25d
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-02-28 18:24:14 -0300

    Select n share code

--- select-n-share/src/app.ts
+++ select-n-share/src/app.ts
@@ -1,1 +1,32 @@
-// Code goes here!
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.querySelector('#address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'KEY_HERE';
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // send to google api
+  axios
+    .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+      if(response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: coordinates,
+        zoom: 8
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
