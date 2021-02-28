cc2518ec5b1a394c3e0a55ba27bc0abcb7522850
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-02-28 18:54:43 -0300

    Cross-component comunication

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -5,8 +5,13 @@ import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{id: 't1', text: 'Text'}];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return <div className="App">
-    <NewTodo />
+    <NewTodo onAddTodo={todoAddHandler} />
     <TodoList items={todos} />
   </div>;
 }
