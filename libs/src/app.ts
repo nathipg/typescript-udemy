56c09d3a0bce1a763adec7d632ccd3ab9f469a8e
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-02-28 17:30:53 -0300

    class-transformer

--- libs/src/app.ts
+++ libs/src/app.ts
@@ -1,7 +1,22 @@
-import _ from 'lodash';
+/* import _ from 'lodash';
 
 declare var GLOBAL: any;
 
 console.log(_.shuffle([1, 2, 3]));
 
-console.log('GLOBAL');
+console.log('GLOBAL'); */
+
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import { Product } from './product.model';
+
+const products = [
+  { title: 'Carpet', price: 5 },
+  { title: 'Book', price: 10 },
+];
+
+const loadedProducts = plainToClass(Product, products);
+
+loadedProducts.forEach(prod => {
+  console.log(prod.getInformation());
+});
