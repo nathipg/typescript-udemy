6feb05eabbf6b16ce57e92507e0122e5e25c32de
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-03-06 20:31:34 -0300

    CRUD

--- node/src/controllers/todos.ts
+++ node/src/controllers/todos.ts
@@ -11,9 +11,51 @@ export const createTodo: RequestHandler = (req, res, next) => {
   TODOS.push(newTodo);
 
   res
-  .status(201)
-  .json({
-    message: 'Todo created',
-    createTodo: newTodo
-  });
+    .status(201)
+    .json({
+      message: 'Todo created',
+      createTodo: newTodo
+    });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res
+    .json({todos: [...TODOS]});
+}
+
+export const updateTodo: RequestHandler<{id: string}> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updatedText = (req.body as {text: string}).text;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if(todoIndex < 0) {
+    throw new Error('Not found');
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res
+    .json({
+      message: 'Todo updated',
+      updatedTodo: TODOS[todoIndex]
+    });
+}
+
+export const deleteTodo: RequestHandler = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if(todoIndex < 0) {
+    throw new Error('Not found');
+  }
+
+  TODOS.splice(todoIndex);
+
+  res
+    .json({
+      message: 'Todo deleted'
+    });
 };
